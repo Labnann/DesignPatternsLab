2ec0cd2ace7925025b3b58b08149aeee4421159d
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-15 14:53:52 +0600

    Rearrange Files

--- Changing Text Behavior/Test/ManualTest/ManualTest.ts
+++ Changing Text Behavior/Test/ManualTest/ManualTest.ts
@@ -1,17 +1,18 @@
-import {BoldWriting, ItalicWriting, PlaneWriting, StrikeThroughWriting, Writing} from "./Writing";
+import {BoldWriting, ItalicWriting, PlaneWriting, StrikeThroughWriting, Writing} from "../../Writing";
 
 let textData: Writing = new PlaneWriting("HelloWorld");
 
 textData = new BoldWriting(textData);
 console.log(textData.readData());
 
+
 textData = new ItalicWriting(textData);
 console.log(textData.readData());
+
 
 let boldStrikeThroughItalicHelloWorld : Writing =
     new BoldWriting(new StrikeThroughWriting( new ItalicWriting( new PlaneWriting("Hello World"))));
 console.log(boldStrikeThroughItalicHelloWorld.readData());
-
 
 
 
